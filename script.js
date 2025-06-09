0a87f090a74539189f2070ef8e05e737361df8b7
Debina <debinabaraili94@gmail.com>
2025-06-09 13:49:12 +0545

    website

---
+++ script.js
@@ -0,0 +1,13 @@
+// -----------FOR ABOUT US SWIPER INITIALIZATION ------------//
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor:true,
+  spaceBetween:true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
